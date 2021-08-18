f20a6a64862799592f129971171b20690747d92f
胡红星 <huhongxing@MacBook-Pro.local>
2021-08-18 14:41:13 +0800

    增加明细表

--- api/src/main/java/com/star/demo/pojo/MtDetail.java
+++ api/src/main/java/com/star/demo/pojo/MtDetail.java
@@ -1,8 +1,21 @@
-package com.star.demo.pojo;/**
-*@program: apache-dubbo-demo
-*@description: 发送明细
-*@author: star.hu
-*@create: 2021-08-18 14:35
-*/
+package com.star.demo.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @program: apache-dubbo-demo
+ * @description: 发送明细
+ * @author: star.hu
+ * @create: 2021-08-18 14:35
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MtDetail {
+
+
+    private long id;
+    private String content;
 }
